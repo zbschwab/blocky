fec95f40346f81d58f0d20383e515398b632f983
Z Schwab <schwabz@grinnell.edu>
2025-09-19 12:26:06 -0500

    added type def to other instances of HasMap

--- src/main/java/com/gamewerks/blocky/engine/Piece.java
+++ src/main/java/com/gamewerks/blocky/engine/Piece.java
@@ -8,7 +8,7 @@ import com.gamewerks.blocky.util.Position;
 
 public class Piece {
     
-    private static HashMap ROTATION_DATA = null;
+    private static HashMap<PieceKind, boolean[][][]> ROTATION_DATA = null;
     
     static {
         try {
